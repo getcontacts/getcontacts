c29b3a331babfd62e08826c387071668f5b0d9f6
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-01-08 16:23:36 -0800

    Added example dir with a small trajectory to test on

---
+++ example/generate_5xnd_contacts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# Example script for generating contact information. 
+
+python3 ../dynamic_contact_networks.py --topology 5xnd_topology.pdb --trajectory 5xnd_trajectory.dcd --all-interactions --output 5xnd_all-contacts.tsv
