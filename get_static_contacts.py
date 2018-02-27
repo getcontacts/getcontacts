89b718c123fbbe69dfc13f979abcd93a8015b515
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-02-27 15:42:26 -0800

    Updated getstaticcontacts to new name

--- get_static_contacts.py
+++ get_static_contacts.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import dynamic_contacts
+import get_dynamic_contacts
 import os
 
 if __name__ == "__main__":
-    dynamic_contacts.main(traj_required=False)
+    get_dynamic_contacts.main(traj_required=False)
 
     # Suppress stdout from vmd as program terminates
     devnull = open('/dev/null', "w")
