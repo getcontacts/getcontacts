d04df3e3c0587750d5c7caf787cba4934759c08b
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-03-06 11:34:20 -0800

    Moving methods into flare

--- contact_calc/flare.py
+++ contact_calc/flare.py
@@ -1,9 +1,34 @@
+"""
+Functions to transform and write flare objects. A "flare" is a json object / dictionary with the following properties
+ * It must have a "edges" key where the value is a list of edge-objects. Each edge-object has the keys
+   - "name1": The identifier of an end-vertex
+   - "name2": The identifier of the other end-vertex
+   - "frames": a list of non-negative integers indicating which time-points or conditions the edge is active
+   - "color": The edge color (not mandatory, default: gray)
+   - "weight": The edge weight (not mandatory, default: 1)
+ * It can have a "trees" key where the value is a list of tree-objects, each with the following keys:
+   - "treeLabel": The name of the tree. If 'default' it will be the default tree when shown on the flareplot page
+   - "treeProperties": A list of objects with "path", and "key" keys used to identify the tree-path of vertices
+ * It can have a "tracks" key where the value is a list of tree-objects, each with the following keys:
+   - "trackLabel": The name of the track. If 'default' it will be the default track when shown on the flareplot page
+   - "trackProperties": A list of objects with "nodeName", "color", and "size" keys indicating the track-objects for
+     each node.
+ * It can have a "frameDict" key (TODO: expand)
+ * It can have a "defaults" key (TODO: expand)
+
+There are three subtypes of flare objects:
+ * Single-flare: has no "frameDict" key and all "frames"-lists have exactly one entry
+ * Time-flare: has no "frameDict" key
+ * Compare-flare: has a "frameDict" key and the frames all index an entry in it
+(TODO: Expand a bit with examples)
+"""
 
 import json
 import re
 import sys
 
-__all__ = ['contacts_to_flare', 'compose_flares']
+__all__ = ['parse_contacts', 'parse_residuelabels', 'create_flare', 'compose_flares', 'write_json']
+
 
 def write_json(flare, fname):
     pretty_json = json.dumps(flare, indent=2)
@@ -13,12 +38,237 @@ def write_json(flare, fname):
         f.write(pretty_json)
 
 
+def is_single_flare(flare):
+    if "frameDict" in flare:
+        return False
+    if any([len(e["frames"] != 1 or e["frames"][0] != 0 for e in flare["edges"])]):
+        return False
+    return True
+
+
+def is_time_flare(flare):
+    if "frameDict" in flare:
+        return False
+    return True
+
+
+def is_compare_flare(flare):
+    if "frameDict" not in flare:
+        return False
+
+    # Check that all frames correspond to a condition in the frameDict
+    conditions = range(len(flare["frameDict"]))
+    for edge in flare["edges"]:
+        if any([f not in conditions for f in edge["frames"]]):
+            return False
+
+    return True
+
+
+def create_flare(contacts, resi_labels):
+    """
+    Creates a flare from a contact-list and residue labels. If `resi_labels` isn't `None` then the "trees" and "tracks"
+    will be generated as well.
+
+    Parameters
+    ----------
+    contacts : list of tuples of (str, str, tuple, tuple [[, tuple], tuple])
+        Each entry specifies a frame-number, an interaction type, and 2 to 4 atom-tuples depending on the interaction
+        type. Water mediated and water-water mediated interactions will have waters in the third and fourth tuples.
+
+    resi_labels : dict of (str : dict of (str : str))
+        Each key is a residue identifier and the associated value is a dictionary with the label, tree-path, and color
+        that are consistent with the format of flareplots.
+
+    Returns
+    -------
+    dict of str : list
+        The types of the list contents varies depending on the key, but the format corresponds to the specifications of
+        jsons used as input for flareplot. For example
+        >>> {
+        >>>   "edges": [
+        >>>     {"name1": "ARG1", "name2": "ALA3", "frames": [0,4,10]},
+        >>>     {"name1": "ALA3", "name2": "THR2", "frames": [1,2]}
+        >>>   ],
+        >>>   "trees": [
+        >>>     {"treeName": "DefaultTree", "treePaths": ["Group1.ARG1", "Group1.THR2", "Group2.ALA3", "Group2.CYS4"]}
+        >>>   ],
+        >>>   "tracks": [
+        >>>     {"trackName": "DefaultTrack", "trackProperties": [
+        >>>       {"nodeName": "ARG1", "size": 1.0, "color": "red"},
+        >>>       {"nodeName": "THR2", "size": 1.0, "color": "red"},
+        >>>       {"nodeName": "ALA3", "size": 1.0, "color": "blue"},
+        >>>       {"nodeName": "CYS4", "size": 1.0, "color": "blue"}
+        >>>     ]}
+        >>>   ]
+        >>> }
+    """
+    ret = {
+        "edges": []
+    }
+
+    # Strip atom3, atom4, and atom names
+    # unique_chains = set([c[2][0] for c in contacts] + [c[3][0] for c in contacts])
+    contacts = [(c[0], c[1], c[2][0:3], c[3][0:3]) for c in contacts]
+
+    resi_edges = {}
+    for contact in contacts:
+        # Compose a key for atom1 and atom2 that ignores the order of residues
+        a1_key = ":".join(contact[2][0:3])
+        a2_key = ":".join(contact[3][0:3])
+        if a1_key == a2_key:
+            continue
+        if a1_key > a2_key:
+            a1_key, a2_key = a2_key, a1_key
+        contact_key = a1_key + a2_key
+
+        # Look up labels
+        if resi_labels:
+            if a1_key not in resi_labels or a2_key not in resi_labels:
+                print("create_flare: Omitting contact "+str(contact)+" as it doesn't appear in flare-label file")
+                continue
+            a1_label = resi_labels[a1_key]["label"]
+            a2_label = resi_labels[a2_key]["label"]
+        else:
+            a1_label = a1_key
+            a2_label = a2_key
+
+        # Create contact_key if it doesn't exist
+        if contact_key not in resi_edges:
+            edge = {"name1": a1_label, "name2": a2_label, "frames": []}
+            resi_edges[contact_key] = edge
+            ret["edges"].append(edge)
+
+        resi_edges[contact_key]["frames"].append(int(contact[0]))
+
+    # Sort edge frames and ensure that there are no duplicates
+    for e in ret["edges"]:
+        e["frames"] = sorted(set(e["frames"]))
+
+    # Create "trees" and "tracks" sections if resi_labels specified
+    if resi_labels is not None:
+        tree = {"treeLabel": "DefaultTree", "treePaths": []}
+        ret["trees"] = [tree]
+
+        track = {"trackLabel": "DefaultTrack", "trackProperties": []}
+        ret["tracks"] = [track]
+
+        for rlabel in resi_labels.values():
+            tree["treePaths"].append(rlabel["treepath"])
+            track["trackProperties"].append({
+                "nodeName": rlabel["label"],
+                "color": rlabel["color"],
+                "size": 1.0
+            })
+
+    return ret
+
+
+def parse_residuelabels(label_file):
+    """
+    Parses a residue-label file and generates a dictionary mapping residue identifiers (e.g. A:ARG:123) to a
+    user-specified label, trees that can be parsed by flareplots, and a color indicator for vertices.
+
+    Parameters
+    ----------
+    label_file : file
+        A flare-label file where each line contains 2-3 columns formatted as
+          - CHAIN:RESN:RESI (e.g. A:ARG:123)
+          - [[TOPLEVEL.]MIDLEVEL.]LABEL (e.g. Receptor.Helix2.2x44)
+          - COLOR (e.g. #FF0000 or white)
+
+    Returns
+    -------
+    dict of str : (dict of str : str)
+        Keys are all residue identifiers and values are dicts that hold both the LABEL by itself (key "label", the full
+        tree-path (key "treepath") and a CSS-compatible color string (key "color").
+
+    Raises
+    ------
+    AssertionError
+        if a residue identifier (CHAIN:RESN:RESI) is specified twice in the file, or if a LABEL appears twice.
+    """
+    if label_file is None:
+        return None
+
+    ret = {}
+    flarelabels = set()  # Only used to check for duplicates
+    for line in label_file:
+        line = line.strip()
+        if not line:
+            continue  # Ignore empty lines
+
+        columns = line.split("\t")
+        residentifier = columns[0]
+        flaretreepath = columns[1] if len(columns) > 1 else columns[0]
+        flarelabel = flaretreepath.split(".")[-1]
+        flarecolor = columns[2] if len(columns) > 2 else "white"
+        if residentifier in ret:
+            raise AssertionError("Residue identifier '"+residentifier+"' appears twice in "+label_file.name)
+        if flarelabel in flarelabels:
+            raise AssertionError("Flare label '"+flarelabel+"' used twice in "+label_file.name)
+
+        ret[residentifier] = {"label": flarelabel, "treepath": flaretreepath, "color": flarecolor}
+        flarelabels.add(flarelabel)
+
+    return ret
+
+
+def parse_contacts(contact_file, itypes):
+    """
+    Parses the contact file and returns it a list of atomic contacts. Atom strings are converted to tuples by splitting
+    on ":".
+
+    Parameters
+    ----------
+    contact_file: file
+        Contact-file generated by dynamic_contacts.py
+
+    itypes: set of str
+        A set of interaction types to retain.
+
+    Returns
+    -------
+    list of tuples of (str, str, tuple, tuple [[, tuple], tuple])
+        Each entry contains a column of the contact_file, e.g.
+        `[("0", "hbbb", ("A", "ARG", "4", "H"), ("A", "PHE", "22", "O")), (..) ]`
+    """
+    def parse_atom(atom_str):
+        atom_tokens = atom_str.split(":")
+        return tuple(atom_tokens)
+
+    ret = []
+    for line in contact_file:
+        line = line.strip()
+        if not line or line[0] == "#":
+            continue  # Ignore empty and commented lines
+
+        # Check number of columns is correct and skip if interaction type is not specified
+        columns = line.split("\t")
+        if not len(columns) in range(4, 7):
+            raise AssertionError("Invalid interaction line: '"+line+"'")
+        if not columns[1] in itypes:
+            continue
+
+        # Parse atoms
+        columns[2] = parse_atom(columns[2])
+        columns[3] = parse_atom(columns[3])
+        if len(columns) >= 5:
+            columns[4] = parse_atom(columns[4])
+        if len(columns) == 6:
+            columns[5] = parse_atom(columns[5])
+
+        ret.append(tuple(columns))
+
+    contact_file.close()
+
+    return ret
+
+
 def compose_flares(singleflares, names):
     """
     Take a list of single-flare files and convert them to a multi-flare file.
-    See https://github.com/GPCRviz/flareplot/tree/master/input for detailed
-    definitions of different types of flare files. Briefly, a single-flare is a
-    flareplot input file where the value of the `frames` attribute of each edge is
+    Briefly, a single-flare is a flareplot input file where the value of the `frames` attribute of each edge is
     `[0]`. This corresponds e.g. to the contact network of a single protein
     structure. A multi-frame has edges with multiple values in the `frames` list,
     but each index has a label specified in a `frameDict` entry. Multi-frames are
@@ -26,9 +276,18 @@ def compose_flares(singleflares, names):
 
     Parameters
     ----------
-    singleflares : list of tuples of (str, str, tuple, tuple [[, tuple], tuple])
+    singleflares : list of dict
+        Single-flare objects representing graph in a certain condition
 
+    names: list of str
+        Names to assign each flare
+
+    Returns
+    -------
+    dict
+        A multiflare object where the entries of the 'frameDict' correspond to the indicated names
     """
+    assert all([is_single_flare(f) for f in singleflares])
 
     ret = {
         "edges": [],
