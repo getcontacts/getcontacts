442088ed9cabc157f08850d65ef7652791e3d6ca
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-04-12 17:18:02 -0700

    Updated example

--- example/generate_5xnd_contacts.sh
+++ example/generate_5xnd_contacts.sh
@@ -2,4 +2,4 @@
 
 # Example script for generating contact information. 
 
-python3 ../dynamic_contacts.py --topology 5xnd_topology.pdb --trajectory 5xnd_trajectory.dcd --all-interactions --output 5xnd_all-contacts.tsv
+get_dynamic_contacts.py --topology 5xnd_topology.pdb --trajectory 5xnd_trajectory.dcd --all-interactions --output 5xnd_all-contacts.tsv
