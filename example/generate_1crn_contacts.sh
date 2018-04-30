db0c717ddc03930c9e3bf0ae09c830cc73cf6092
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-04-30 14:20:33 -0700

    Updated arguments in examples

--- example/generate_1crn_contacts.sh
+++ example/generate_1crn_contacts.sh
@@ -4,4 +4,4 @@
 
 pymol -c -d 'fetch 1crn, async=0; h_add; save 1crn_h.pdb'
 rm -f 1crn.cif
-python3 ../static_contacts.py --topology 1crn_h.pdb --all-interactions --output 1crn_all-contacts.tsv
+python3 ../get_static_contacts.py --structure 1crn_h.pdb --itypes all --output 1crn_all-contacts.tsv
--- example/generate_5xnd_contacts.sh
+++ example/generate_5xnd_contacts.sh
@@ -2,4 +2,4 @@
 
 # Example script for generating contact information. 
 
-get_dynamic_contacts.py --topology 5xnd_topology.pdb --trajectory 5xnd_trajectory.dcd --all-interactions --output 5xnd_all-contacts.tsv
+get_dynamic_contacts.py --topology 5xnd_topology.pdb --trajectory 5xnd_trajectory.dcd --itypes all --output 5xnd_all-contacts.tsv
