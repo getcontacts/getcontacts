c6a65490481c52ce4aa9f22d4eebf338351cc87c
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-05-05 21:59:50 -0700

    Added encodings

--- get_dynamic_contacts.py
+++ get_dynamic_contacts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Computes non-covalent contact networks for MD simulations.
 
--- get_static_contacts.py
+++ get_static_contacts.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Computes non-covalent contact networks for a protein structure.
 
