7240cb65ea69d0252163510f73456598888369a2
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2019-03-16 07:00:38 -0700

    Import fix

--- example/1crn/generate_1crn_contacts.sh
+++ example/1crn/generate_1crn_contacts.sh
@@ -4,4 +4,4 @@
 
 pymol -c -d 'fetch 1crn, async=0; h_add; save 1crn_h.pdb'
 rm -f 1crn.cif
-python3 ../get_static_contacts.py --structure 1crn_h.pdb --itypes all --output 1crn_all-contacts.tsv
+python ../../get_static_contacts.py --structure 1crn_h.pdb --itypes all --output 1crn_all-contacts.tsv
